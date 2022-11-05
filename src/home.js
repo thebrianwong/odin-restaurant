1b255bb487bac380b58fe4221cfa3d135831b91f
Brian Wong <brianw7218@gmail.com>
2022-11-05 15:19:07 -0700

    Load homepage content with JS

---
+++ src/home.js
@@ -0,0 +1,79 @@
+const displayHomeComponent = () => {
+    const content = document.querySelector("#content");
+    content.innerHTML = `
+    <div class="header-container">
+    <div class="icon"><img src="./logo.PNG" alt="Fuds logo with a fork, spoon, knife, and gun."></div>
+    <div class="tagline">
+        <h1>A JOURNEY IN FOOD-TASTE FROM MOUTH TO TOILET,<br>
+            TRAVELING THE ULTIMATE EXPANSE OF THE GREEKO-JAPO<br>
+            PAN-AMERICAN DINING EXPERIENCES.</h1>
+    </div>
+</div>
+<nav class="nav-container">
+    <button class="nav-button">Home</button>
+    <button class="nav-button">Menu</button>
+    <button class="nav-button">Apply</button>
+    <button class="nav-button">Credits</button>
+</nav>
+<div class="main-content">
+    <div class="reviews-container">
+        <div class="review" id="review-1">
+            <p>
+                <span class="italics">
+                    "Why did you take this picture?<br>
+                    I said I was going to take off my sunglasses after I took a bite."
+                </span>
+                <br>
+                <span class="bold">
+                    - MysteryGuitarMan
+                </span>
+            </p>
+            <img src="./review_1.jpg" alt="Sunglasses-wearing man eats a piece of bread.">
+        </div>
+        <div class="review" id="review-2">
+            <img src="./review_2.jpg" alt="Two women eating pizza, with an out-of-focus child in the background.">
+            <p>
+                <span class="italics">
+                    "When is this gonna to be over?<br>
+                    I wanna go home and watch Paw Patrol."
+                </span>
+                <br>
+                <span class="bold">
+                    - Bored Child in the Background
+                </span>
+            </p>
+        </div>
+        <div class="review" id="review-3">
+            <p>
+                <span class="italics">
+                    0/5<br>
+                    "The food here is awful.<br>
+                    I literally could not take a single bite."
+                </span>
+                <br>
+                <span class="bold">
+                    - Utensil Illiterate Yelp Reviewer
+                </span>
+            </p>
+            <img src="./review_3.jpg" alt="Man having trouble eating steak without using any utensils, just his two bare hands.">
+        </div>
+    </div>
+</div>
+<div class="footer-container">
+    <div class="contact-container">
+        <p><span class="bold">EMAIL:</span>
+            FUDSMENU@GMAIL.COM
+        </p>
+    </div>
+    <div class="hours-container">
+        <p><span class="bold">HOURS:</span>
+            MONDAY - FRIDAY: 5AM - 4AM
+            SATURDAY: 4PM - 4:45PM
+            SUNDAY: CLOSED - 2PM
+        </p>
+    </div>
+</div>
+    `
+}
+
+export { displayHomeComponent };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("test");
+import { displayHomeComponent } from "./home";
+
+displayHomeComponent();
