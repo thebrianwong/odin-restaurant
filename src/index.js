c7121fe4617a27e0da6023b45488a90675085ef3
Brian Wong <brianw7218@gmail.com>
2022-11-05 16:33:33 -0700

    Add clicker to the menu button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import { initialLoadComponent } from "./initial";
+import { loadMenuComponent } from "./menu";
 
 initialLoadComponent();
+
+const addMenuButtonClicker = () => {
+    const menuButton = document.querySelector("#menu-button");
+    menuButton.addEventListener("click", loadMenuComponent)
+}
+
+addMenuButtonClicker();
